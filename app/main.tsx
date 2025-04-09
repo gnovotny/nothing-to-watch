913119e427ca54bc454a7d2bdf0df083732dd256
gnovotny <g.novotny@gmx.ch>
2025-04-09 18:41:32 +0200

    l

--- app/main.tsx
+++ app/main.tsx
@@ -1,3 +1,6 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './app'
 import { animateHtmlTitleSuffix } from './utils/anim'
 import { initVoroforce } from './voroforce'
 import './index.css'
@@ -11,11 +14,11 @@ window.addEventListener('load', async () => {
   }
 
   // biome-ignore lint/style/noNonNullAssertion: exists
-  // createRoot(document.getElementById('root')!).render(
-  //   <StrictMode>
-  //     <App />
-  //   </StrictMode>,
-  // )
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
 
   animateHtmlTitleSuffix()
 })
