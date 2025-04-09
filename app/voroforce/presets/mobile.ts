1bb47221e97d6b87c013b781919d2fde319b8547
gnovotny <g.novotny@gmx.ch>
2025-04-09 19:25:33 +0200

    l

--- app/voroforce/presets/mobile.ts
+++ app/voroforce/presets/mobile.ts
@@ -1,4 +1,4 @@
-import postFrag from '../config/display/post.frag'
+import postFrag from '√/display/scene/shaders/post/post-default.frag'
 
 export default {
   // cells: 10000,
@@ -11,58 +11,6 @@ export default {
       post: {
         enabled: true,
         fragmentShader: postFrag,
-        uniforms: {
-          fBaseColor: {
-            value: [0.005, 0.005, 0.005],
-            animatable: true,
-            themes: {
-              default: {
-                value: [0.005, 0.005, 0.005],
-              },
-              light: {
-                value: [0.995, 0.995, 0.995],
-              },
-            },
-          },
-          fAlphaStrength: {
-            // value: 0.3,
-            value: 1,
-            animatable: true,
-            modes: {
-              default: {
-                // value: 0.3,
-                value: 1,
-              },
-              select: {
-                value: 1,
-              },
-            },
-          },
-          fEdgeStrength: {
-            // value: 0.3,
-            value: 1,
-            animatable: true,
-            modes: {
-              default: {
-                // value: 0.3,
-                value: 1,
-              },
-              select: {
-                value: 1,
-              },
-            },
-          },
-          iChannel0: {
-            src: '/assets/rust.jpg',
-            width: 512,
-            height: 512,
-          },
-          iChannel1: {
-            src: '/assets/noise.png',
-            width: 256,
-            height: 256,
-          },
-        },
       },
     },
   },
