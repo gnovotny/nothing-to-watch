90d438bae97b131c4645e27a94f47baaaaff6c1c
gnovotny <g.novotny@gmx.ch>
2025-05-01 21:30:33 +0200

    l

--- app/cmps/modals/film/view/film-view.tsx
+++ app/cmps/modals/film/view/film-view.tsx
@@ -6,38 +6,31 @@ import { FilmRatingGauge } from '../shared/film-rating-gauge'
 import {} from 'lucide-react'
 import { store } from '@/store'
 import {} from '../../../ui/tooltip'
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 
 export const FilmView = ({
   film,
   className = '',
 }: { film?: Film; className?: string }) => {
+  const filmRef = useRef<Film>(undefined)
   const ua = store((state) => state.ua)
 
   const [backdropHidden, setBackdropHidden] = useState(true)
   const [backdropErrored, setBackdropErrored] = useState(true)
 
-  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
-    setBackdropHidden(true)
+    if (filmRef.current?.tmdbId !== film?.tmdbId) {
+      setBackdropHidden(true)
+    }
+    filmRef.current = film
   }, [film])
 
   const isIOS = useMemo(() => ua.getOS()?.name === 'iOS', [ua])
 
   if (!film) return
+
   return (
-    <div
-      className={cn(
-        'landscape:h-full',
-        // 'bg-center bg-cover bg-no-repeat',
-        className,
-      )}
-      // style={{
-      //   // backgroundPosition: 'calc((((50vw / 2.222222) - 20px) / 1.5) / 2) 0',
-      //   backgroundImage: `url('${config.backdropBaseUrl}${film.backdrop}')`,
-      //   // backgroundBlendMode: 'lighten',
-      // }}
-    >
+    <div className={cn('landscape:h-full', className)}>
       {!isIOS && (
         <img
           className={cn(
@@ -58,36 +51,9 @@ export const FilmView = ({
           }}
         />
       )}
-      <div
-        className='relative z-1 flex h-full w-full flex-col'
-        // style={{
-        //   backgroundImage:
-        //     'linear-gradient(to right, var(--background) calc((25vw - 170px) - 340px), color-mix(in oklab, var(--background) 64%, transparent) 50%, color-mix(in oklab, var(--background) 64%, transparent) 100%)',
-        // }}
-      >
-        <div
-          className={cn(
-            'max-h-64 w-full md:max-h-auto',
-            // 'bg-background/60',
-            {},
-          )}
-          // style={{
-          //   backgroundPosition: 'center center',
-          //   // backgroundPosition: 'calc((((50vw / 2.222222) - 20px) / 1.5) / 2) 0',
-          //   backgroundImage: `url('${config.backdropBaseUrl}${film.backdrop}')`,
-          //   backgroundSize: 'cover',
-          //   backgroundRepeat: 'no-repeat',
-          //   // backgroundBlendMode: 'lighten',
-          // }}
-        >
-          <div
-            className='flex h-full w-full flex-row gap-6 p-4 md:p-6 lg:p-6 xl:p-9'
-            // style={{
-            //   backgroundImage:
-            //     'linear-gradient(to right, var(--background) calc((25vw - 170px) - 340px), color-mix(in oklab, var(--background) 64%, transparent) 50%, color-mix(in oklab, var(--background) 64%, transparent) 100%)',
-            // }}
-          >
-            {/*<FilmPoster film={film} className='aspect-[2/3] h-full rounded-sm' />*/}
+      <div className='relative z-1 flex h-full w-full flex-col'>
+        <div className={cn('max-h-64 w-full md:max-h-auto', {})}>
+          <div className='flex h-full w-full flex-row gap-6 p-4 md:p-6 lg:p-6 xl:p-9'>
             <div className='flex w-full flex-col justify-between gap-9'>
               <div className='flex w-full flex-col gap-3'>
                 <div className='relative flex w-full flex-row items-start justify-between gap-3 pr-28'>
