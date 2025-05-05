d5e5a2eb21581285f2d776bb4cd989e1d9f763e3
gnovotny <g.novotny@gmx.ch>
2025-05-05 13:08:04 +0200

    type fix

--- app/cmps/modals/modals-root.tsx
+++ app/cmps/modals/modals-root.tsx
@@ -1,12 +1,13 @@
-import { useShallowState } from '../../store'
-import { FadeTransition } from '../common/transition'
-import { lazy } from 'react'
+import {useShallowState} from '../../store'
+import {FadeTransition} from '../common/transition'
+import {lazy} from 'react'
+import {VOROFORCE_MODE} from "@/vf";
 
 const Modals = lazy(() => import('./modals'))
 
 function ModalsRoot() {
   const canLoad = useShallowState(
-    ({ mode, preset }) => mode !== 'intro' && Boolean(preset),
+    ({ mode, preset }) => mode !== VOROFORCE_MODE.intro && Boolean(preset),
   )
 
   return (
